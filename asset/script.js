d39475a5751066839fd1b6def7438e6d2c03aa06
varangXI <salbiev.t99@mail.ru>
2022-09-22 09:53:19 +0300

    ready

--- asset/script.js
+++ asset/script.js
@@ -1,24 +1,107 @@
-$('#form').submit(function(e){
-    // e.preventDefault();
-    let th = $(this);
+console.log("Script file is working");
 
-    $.ajax({
-        url: 'index.php', 
-        type: 'POST',
-        data: th.serialize(),
-        beforeSend: function()
+const form = document.querySelector('#form');
+const telSelector = form.querySelector('input[name="phone"]');
+const telSelector2 = form.querySelector('input[name="phone2"]');
+const inputMask = new Inputmask('+7 (999) 999-99-99');
+inputMask.mask(telSelector);
+inputMask.mask(telSelector2);
+
+const validation = new JustValidate('.form-fields');
+
+validation
+    .addField('#name', [
         {
-            $('.loader').fadeIn();
-        }, 
-        success: function()
-        {
-            $('.loader').fadeOut('slow', function(){
-                console.log("сообщение отправлено");
-            });
+            rule: 'minLength',
+            value: 3,
         },
-        error: function()
         {
-            alert('ошибка отправки');
+            rule: 'maxLength',
+            value: 30,
+        },
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '*'   //не введено
         }
-    });
+    ])
+    .addField('#surname', [
+        {
+            rule: 'minLength',
+            value: 3,
+        },
+        {
+            rule: 'maxLength',
+            value: 30,
+        },
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '*'   //не введно
+        }
+    ])
+    .addField('#patronymic', [
+        {
+            rule: 'minLength',
+            value: 3,
+        },
+        {
+            rule: 'maxLength',
+            value: 30,
+        },
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '*'   //не введно
+        }
+    ])
+    .addField('#email', [
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '*', //не введено 
+        },
+        {
+            rule: 'email',
+            value: true,
+            errorMessage: '*', // введен некорректно
+        },
+    ])
+    .addField('#phone', [
+        {
+            rule: 'required',
+            value: true,
+            errorMessage: '*', //не введен
+        },
+        {
+            rule: 'function',
+            validator: function() {
+                const phone = telSelector.inputmask.unmaskedvalue();
+                return phone.length === 10;
+            },
+            errorMessage: '*', //Некорректный телефон
+        },
+    ])
+    .onSuccess((event) => {
+    console.log('Валидация прошла, форма отправляется', event);
+
+    let formData = new FormData(event.target);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log('Отправлено!!!!!!!');
+            }
+        }
+    }
+
+    xhr.open('POST', 'mail.php', true);
+    xhr.send(formData);
+
+    event.target.reset();
 });
+
+
+
---
+++ mail.php
@@ -0,0 +1,54 @@
+<?php
+// Файлы phpmailer
+require 'libs/PHPMailer.php';
+require 'libs/SMTP.php';
+require 'libs/Exception.php';
+
+$title = "Тема письма";
+
+$c = true;
+
+// Формирование письма
+$title = "Заголовок";
+foreach ($_POST as $key => $value) {
+    if ($value != "") {
+        $body .= "
+    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #ffffff;">') . "
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+    </tr>
+    ";
+    }
+}
+
+$body = "<table style='width: 100%;'>$body</table>";
+
+//  PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+
+    // привязка почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервер почты
+    $mail->Username   = 'salbiev.t99@gmail.com'; // Логин на почте
+    $mail->Password   = 'epoowpdmhqlujknm'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+
+    $mail->setFrom('salbiev.t99@gmail.com', 'Message from mika'); // Адрес почты и заголовок
+
+    // Получатель письма
+    $mail->addAddress('salbiev.t99@mail.ru');
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;
+
+    $mail->send();
+} catch (Exception $e) {
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
